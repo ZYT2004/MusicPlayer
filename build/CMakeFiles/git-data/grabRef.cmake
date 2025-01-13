0d9799bf4897cc6fdc79712b6385af0e7dc6b757
郑伊恬 <119641983+ZYT2004@users.noreply.github.com>
2025-01-13 20:21:07 +0800

    更新项目版本和构建路径，调整Git引用以支持新分支

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Users/14913/Downloads/sdspi/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "D:/.git")
+set(GIT_DIR "D:/Users/14913/Downloads/sdspi/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
